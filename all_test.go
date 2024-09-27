5798d0fdc77adb37541f9e81bb838af41cecd6e2
snowmerak <snowmerak@outlook.com>
2024-09-27 23:21:57 +0900

    create test

---
+++ all_test.go
@@ -0,0 +1,64 @@
+package all_test
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/snowmerak/all"
+)
+
+const (
+	Subscribers = 1000
+	Messages    = 10000
+	Buffer 	= 100
+)
+
+func BenchmarkAllLinkedList(b *testing.B) {
+	ll := all.New[[]byte](Buffer)
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < Subscribers; i++ {
+		wg.Add(1)
+		ll.Subscribe(func(value []byte, closed bool) {
+			if closed {
+				wg.Done()
+			}
+
+			_ = value
+		})
+	}
+
+	for i := 0; i < Messages; i++ {
+		ll.Push([]byte("hello"))
+	}
+	ll.Close()
+
+	wg.Wait()
+}
+
+func BenchmarkChannel(b *testing.B) {
+	chList := make([]chan []byte, Subscribers)
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < Subscribers; i++ {
+		wg.Add(1)
+		chList[i] = make(chan []byte, Buffer)
+		go func(ch chan []byte) {
+			defer wg.Done()
+			for mesg := range ch {
+				_ = mesg
+			}
+		}(chList[i])
+	}
+
+	for i := 0; i < Messages; i++ {
+		for j := 0; j < Subscribers; j++ {
+			chList[j] <- []byte("hello")
+		}
+	}
+	for i := 0; i < Subscribers; i++ {
+		close(chList[i])
+	}
+
+	wg.Wait()
+}
